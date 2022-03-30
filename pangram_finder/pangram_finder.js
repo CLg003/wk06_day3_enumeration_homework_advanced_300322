7d571589a112a60dfaf16fa5beae6982667f46dc
CLg003 <clairemjlaing@gmail.com>
2022-03-30 18:35:58 +0100

    Pangram lowercase and mixed case tests passed.

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,11 @@
 const PangramFinder = function (phrase) {
+  this.phrase = phrase;
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
 }
 
-PangramFinder.prototype.isPangram = function () {
-
+PangramFinder.prototype.isPangram = function() {
+  const lowerCasePhrase = this.phrase.toLowerCase();
+  return this.alphabet.every(letter => lowerCasePhrase.includes(letter));
 }
 
 module.exports = PangramFinder;
