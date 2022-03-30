2e3f43565b0361ed1780b8a7488b101be46aa410
CLg003 <clairemjlaing@gmail.com>
2022-03-30 19:02:07 +0100

    Isogram finder tests all passed.

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,11 @@
 const IsogramFinder = function (word) {
-
+    this.word = word;
 }
 
-IsogramFinder.prototype.isIsogram = function () {
-
+IsogramFinder.prototype.isIsogram = function() {
+    const lowerCaseWord = this.word.toLowerCase();
+    const wordArray = lowerCaseWord.split('');
+    return wordArray.every(letter => wordArray.indexOf(letter) === wordArray.lastIndexOf(letter));
 }
 
 module.exports = IsogramFinder;
